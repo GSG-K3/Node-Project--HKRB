d5ae42e38e8aa02a8ac35dfb848048885250d20a
rubasider <rubashka12@gmail.com>
2020-02-11 14:43:22 +0200

    dom #2

---
+++ public/index.js
@@ -0,0 +1,11 @@
+for (let i = 0; i < 4; i++) {
+    const firstDiv = document.createElement("div")
+    const secondDiv = document.createElement("div")
+    const bookImg = document.createElement("img")
+    const bookName = document.createElement("h2")
+    const bookAuther = document.createElement("p")
+
+    bookImg.className = "book-logo"
+    bookName.className = "book_name"
+    bookAuther.className = "book_auther"
+}
