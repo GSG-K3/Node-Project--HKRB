ec4b820110bcabe11e030ac36490f87b4c178906
rubasider <rubashka12@gmail.com>
2020-02-11 15:06:52 +0200

    dom #2

--- public/index.js
+++ public/index.js
@@ -1,11 +1,12 @@
+// eslint-disable-next-line no-plusplus
 for (let i = 0; i < 4; i++) {
-    const firstDiv = document.createElement("div")
-    const secondDiv = document.createElement("div")
-    const bookImg = document.createElement("img")
-    const bookName = document.createElement("h2")
-    const bookAuther = document.createElement("p")
+    const firstDiv = document.createElement('div');
+    const secondDiv = document.createElement('div');
+    const bookImg = document.createElement('img');
+    const bookName = document.createElement('h2');
+    const bookAuther = document.createElement('p');
 
-    bookImg.className = "book-logo"
-    bookName.className = "book_name"
-    bookAuther.className = "book_auther"
+    bookImg.className = 'book-logo';
+    bookName.className = 'book_name';
+    bookAuther.className = 'book_auther';
 }
