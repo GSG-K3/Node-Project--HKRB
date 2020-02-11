f723a7b0e404536f2f3126eef3a8830deac0fb13
Hussein-shahatet97 <hussein.sh1997@hotmail.com>
2020-02-11 16:32:59 +0200

    error handling #8

--- src/server.js
+++ src/server.js
@@ -1,17 +1,41 @@
 const express = require('express');
 const request = require('request');
-
 const app = express();
 
 
-app.get('/:name', (req, res,body) => {
+app.get('/', (req, res) => {
+    let url = 'https://www.googleapis.com/books/v1/volumes?q=""';
+    request(url, (error, response) => {
+        response = JSON.parse(response.body);
+        res.send(response);
+
+    });
+
+
+});
+
+// search of input values(Book name)from dom.
+app.get('/search/:name', (req, res) => {
     let url = `https://www.googleapis.com/books/v1/volumes?q=${req.params.name}`
     request(url, (error, response) => {
-        response=JSON.parse(response.body);
+        response = JSON.parse(response.body);
         res.send(response);
 
     });
 });
+
+// check the foundation of pages
+app.use((req, res) => {
+    res.status(404).send("Page not found 404");
+});
+
+// check server errors 
+app.use((error, req, res, next) => {
+    res.status(500).send("internal server error");
+    next();
+});
+
+
 
 app.listen(3000, () => {
     console.log('This server is running');
