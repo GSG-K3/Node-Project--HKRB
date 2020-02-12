12d03687f94c27f99526d83cbc47d3ac441e127a
bayanseder <bayansider88@gmail.com>
2020-02-12 16:16:04 +0200

    write read me

--- public/index.js
+++ public/index.js
@@ -1,25 +1,32 @@
-// import { response } from "express";
-
-// eslint-disable-next-line no-plusplus
-
 const xhr = new XMLHttpRequest();
 
 const inputValue = document.getElementById('inputSearch');
+const buttonSearch = document.getElementById('buttonSearch');
+buttonSearch.addEventListener("click", (event) => {
+    event.preventDefault()
+    const bookvalue = inputValue.value.trim();
+    const url = `https://www.googleapis.com/books/v1/volumes?q=${bookvalue}`;
+    apicall(url, bookfunction, bookvalue);
+
+})
 const bookList = document.getElementById('bookList');
-const searchButton = document.getElementById('buttonSearch');
 
 
-const apicall = (url, callback) => {
+const apicall = (url, callback, bookvalue) => {
     xhr.onreadystatechange = () => {
         if (xhr.status === 200 && xhr.readyState === 4) {
             const response = JSON.parse(xhr.responseText);
+
             if (callback) callback(response);
         }
     };
-    xhr.open('GET', url);
+    xhr.open('GET', `/search=${bookvalue}`, true);
+
     xhr.send();
 };
 const bookfunction = (response) => {
+    bookList.innerText = "";
+
     for (let i = 0; i < 4; i++) {
         const firstDiv = document.createElement('div');
         const bookItem = document.createElement('div');
@@ -27,24 +34,21 @@ const bookfunction = (response) => {
         const bookImg = document.createElement('img');
         const bookName = document.createElement('h2');
         const bookAuther = document.createElement('p');
+        const bookDescription = document.createElement('p');
 
         bookImg.className = 'book-logo';
         bookName.className = 'book_name';
         bookAuther.className = 'book_auther';
-        bookName.innerHTML = response.items[i].title;
-        // bookItem.src=response.items.imageLinks.smallThumbnail
+        bookName.innerHTML = response.items[i].volumeInfo.title;
+        bookAuther.innerHTML = response.items[i].volumeInfo.authors[0];
+        bookImg.src = response.items[i].volumeInfo.imageLinks.thumbnail;
+        bookDescription.innerHTML = response.items[i].volumeInfo.description;
         firstDiv.appendChild(bookImg);
         secondDiv.appendChild(bookName);
         secondDiv.appendChild(bookAuther);
         bookItem.appendChild(firstDiv);
         bookItem.appendChild(secondDiv);
         bookList.appendChild(bookItem);
+        secondDiv.appendChild(bookDescription);
     }
 };
-searchButton.addEventListener('click', (event) => {
-    const bookvalue = inputValue.value.trim();
-    const url = `https://www.googleapis.com/books/v1/volumes?q=${bookvalue}`;
-    console.log('hi');
-    apicall(url, bookfunction);
-    bookvalue.value = '';
-});
